0d06d6d8fbeab0f706f1c07f444cffe8697feaea
dmibre <dmibre@greeny.cs.tlu.ee>
2016-02-18 13:04:20 +0200

    Added this functionality to the app

---
+++ app.php
@@ -0,0 +1,46 @@
+<?php
+	//check if there is variable in the URL
+	if(isset($_GET["message"])){
+		
+		//only if there is message in the URL
+		//echo "there is message";
+		
+		//if its empty
+		if(empty($_GET["message"])){
+			//it is empty
+			echo "Please enter the message!";
+		}else{
+			//its not empty
+			echo "Message: ".$_GET["message"]."<br>";
+		}
+		
+	}else{
+		//echo "there is no such thing as message";
+	}
+	
+	//Getting the message from address
+	// if there is ?name= .. then $_GET["name"]
+	//$my_message = $_GET["message"];
+	//$to = $_GET["to"];
+	
+	
+	//echo "My message is ".$my_message." and is to ".$to;
+	
+?>
+
+<h2> First application </h2>
+
+<form method="get">
+   <label for="to">to:* <label><br>
+   <input type="text" name="to"><br><br>
+   
+   <label for="message">Message:* <label><br>
+   <input type="text" name="message"><br><br>
+   
+   <label for="email">email:* <label><br>
+   <input type="text" name="email"><br><br>
+   
+   <!-- This is the save button--> 
+   <input type="submit" value="Save to DB">
+   
+<form>
