613954d2d6bac61389276a5faabcd8317a7c998d
laukoi <laukoi@greeny.cs.tlu.ee>
2016-02-24 18:26:17 +0200

    did homework

--- app.php
+++ app.php
@@ -1,23 +1,105 @@
+
+<html>
+<head>
+<style>
+h2 {
+    color: rgb(159,107,63);
+    font-family: Arial;
+    font-size: 250%;
+
+}
+h3 {
+    color: rgb(84,84,84);
+    font-family: Arial;
+
+
+}
+p  {
+    color: rgb(75,106,136);
+    font-family: Arial;
+    font-size:;
+}
+</style>
+</head>
+<body>
+
+</body>
+</html>
+
+<h2 style="text-align:center;">Grocery list</h2>
+<h3 style="text-align:center;"><i>For all you scatterbrained idiots!</i></h3>
+
+<img src="https://d30y9cdsu7xlg0.cloudfront.net/png/28774-200.png">
+
+<br>
+
+
+</br>
+
+<form>
+
+<form method="get">
+
+  <label for="message">#1:* <label>
+  <input type="text" name="message"<br><br>
+
+	<label for="message">#2:* <label>
+	<input type="text" name="message"<br><br>
+
+
+	<label for="to">#3:* <label>
+	<input type="text" name="to"<br><br>
+
+
+  <label for="from">#4:* <label>
+	<input type="text" name="from"<br><br>
+
+
+
+	<!-- This is the save buttn-->
+	<input type="Submit" value="Save to DB">
+
+
+<form>
+	<p style="text-align:center;"><b> Add the items you need and they will appear
+	as a list below </b></p>
+
 <?php
 
 	//check if there is variable in the URL
 	if (isset ($_GET ["message"])) {
-
 		//only if there is message in the URL
 		//echo "there is message";
-
 		//if its empty
 		if (empty($_GET ["message"])){
 			//it is empty
-			echo "Please enter the message <br>";
+			echo "Enter the item <br>";
 		}else{
 			//its not empty
-			echo "Message: ".$_GET["message"]."<br>";
+			echo "#1: ".$_GET["message"]."<br>";
 		}
 
 	}else{
 		//echo "there is no such thing as message";
 	}
+
+		//check if there is variable in the URL
+		if (isset ($_GET ["message"])) {
+			//only if there is message in the URL
+			//echo "there is message";
+			//if its empty
+			if (empty($_GET ["message"])){
+				//it is empty
+				echo "Enter the item <br>";
+			}else{
+				//its not empty
+				echo "#2: ".$_GET["message"]."<br>";
+			}
+
+		}else{
+			//echo "there is no such thing as message";
+
+		}
 
 	//check if there is variable in the URL
 	if (isset ($_GET ["to"])) {
@@ -27,11 +109,15 @@
 
 		//if its empty
 		if (empty($_GET ["to"])){
+
 			//it is empty
-			echo "Please enter the recipient <br> ";
+			echo "Enter the item <br> ";
+
 		}else{
+
 			//its not empty
-			echo "to: ".$_GET["to"]."<br>";
+
+			echo "#3: ".$_GET["to"]."<br>";
 		}
 
 	}else{
@@ -46,10 +132,10 @@
 		//if its empty
 		if (empty($_GET ["from"])){
 			//it is empty
-			echo "Please enter your name <br>";
+			echo "Enter the item <br>";
 		}else{
 			//its not empty
-			echo "From: ".$_GET["from"]."<br>";
+			echo "#: ".$_GET["from"]."<br>";
 		}
 
 	}else{
@@ -67,31 +153,3 @@
 
 
 ?>
-
-<h2> First Applications </h2>
-
-
-
-<form>
-
-<form method="get">
-
-  <label for="message">Message:* <label>
-  <input type="text" name="message"<br><br>
-
-	<label for="to">To:* <label>
-	<input type="text" name="to"<br><br>
-
-
-  <label for="from">From:* <label>
-	<input type="text" name="from"<br><br>
-
-
-	<!-- This is the save buttn-->
-	<input type="Submit" value="Save to DB">
-
-
-<form>
-	<p> My idea: Interactive grocery list you can edit and add items there
-  until you finally go to the store. The four fields are vegetables,
-meatproducts, sweets and then the wheatproducts</p>
