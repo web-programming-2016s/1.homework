90a6b4ba860e8f3b9dc5ddf6f9b8117a6e6e162f
qpelit <qpelit@greeny.cs.tlu.ee>
2016-02-24 18:29:42 +0200

    Added some text and radio to Register Page

--- app.php
+++ app.php
@@ -1,14 +1,16 @@
 <html>
 <body>
     <h2> Forum Page </h2>
-<form action="functioncalling.php">
-    <b>Name     </b>     <input type="text" name="userName" /> <br>
-    <b>Password </b><input type="text" name="userPassword" /><br>
+<form method="get">
+    <label for ="name">Name        </label>  <input type="text" name="name" /> <br>
+    <label for ="password">Password</label>  <input type="text" name="password" /><br>
 <input type="submit" class="button" name="login" value="Log In" />
-<input type="submit" class="button" name="login" value="Register" />
+<input type="submit" class="button" name="register" value="Register" />
 </form>
-    
+    </body></html>    
 <?php
+$user_name = $_GET["name"];
+$user_password = $_GET["password"];
 
 
 ?>
---
+++ registerPage.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+    <h1> Forum Page </h1>
+        <h2> Register Page </h2>
+<form method="get">
+    <label for ="name">Name*        </label>  <input type="text" name="name" /> <br>
+    <label for ="password">Surname</label>  <input type="text" name="surname" /><br>
+    <label for ="name">Mail*       </label>  <input type="text" name="mail" /> <br>
+    <label for ="password">Password*</label>  <input type="text" name="password" /><br>
+    <label for ="gender">Gender</label><br><input type="radio" name="gender" value="male" checked> Male<br>
+  <input type="radio" name="gender" value="female"> Female<br>
+
+<input type="submit" class="button" name="register" value="Submit" />
+</form>
+    </body></html>    
+<?php
+
+?>
