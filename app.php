9cd5bc0fa8e2a577697060d34ac5f997bfecfc16
qpelit <qpelit@greeny.cs.tlu.ee>
2016-02-24 21:45:57 +0200

    fixed problems

--- app.php
+++ app.php
@@ -3,7 +3,7 @@
     <h2> Forum Page </h2>
 <form method="get">
     <label for ="name">Name*        </label>  <input type="text" name="name" /> <br>
-    <label for ="password">Password*</label>  <input type="text" name="password" /><br>
+    <label for ="password">Password*</label>  <input type="password" name="password" /><br>
 <input type="submit" class="button" name="login" value="Log In" />
     
    
