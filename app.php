d7bcec999f312c215798e2089a4974472742b4ea
marvin <marvin@greeny.cs.tlu.ee>
2016-02-24 18:53:35 +0200

    bmi fields and description added

--- app.php
+++ app.php
@@ -1,90 +1,146 @@
+<style>
+body {
+	color:rgb(41,91,81);
+	background-color:rgb(201,245,225);
+	font-family:sans-serif;
+	font-size:10pt;
+	text-align:center;
+	margin:100px;}
+	}
+h2   {
+	font-size:26pt;
+	color:#54b352; 
+	font-family:sans-serif;
+	text-align:center;}
+p    {
+	color:green; 
+	font-family:sans-serif; 
+	font-size:8pt;
+	text-align:justify;}
+	
+
+</style>
+
+<h2> First application (BMI)</h2>
+
+<p>My idea is to create a personal app for tracking your body mass index (BMI). After inputing your parameters, the app will let you know if your body mass index
+is normal or should you lose/gain weight. The app could track user's progress 
+and leave positive comments /send reminders. The app could be accessible after entering a password.</p>
+
+<form method="get"
+	<label for="gender"> I am:*</label><br>
+	<input type="radio" name="gender" value="Male"> Male<br>
+	<input type="radio" name="gender" value="Female"> Female<br>
+</form>
+
 <?php
 
 
 	//check if there is variable in the URL
 	
 	
-	//for Message* field:
-	if(isset($_GET["message"])){
+	//for Gender* field:
+	if(isset($_GET["gender"])){
 		
-		//only if there is message in the URL
-		//echo "there is message";
+		//only if there is gender typed in the URL
+		//echo "there is gender";
 		
 		//if it's empty
-		if(empty($_GET["message"])){
+		if(empty($_GET["gender"])){
 			//it is empty
-			echo "Please enter the message!";
+			echo "Please choose your gender!";
 	}else{
 			//its not empty
-			echo "Message: ".$_GET["message"]."<br>"; 
+			echo "Gender: ".$_GET["gender"]."<br>"; 
 		}
 		
 	}else{
-		//echo "there is no such thing as message";
+		//echo "there is no such thing as gender typed";
 	}
-	
-	//for To* field:
-	if(isset($_GET["to"])){
-		
-		//only if there is to* in the URL
-		//echo "there is addressee";
-		
-		//if it's empty
-		if(empty($_GET["to"])){
-			//it is empty
-			echo "Please enter the addressee!";
-	}else{
-			//its not empty
-			echo "To: ".$_GET["to"]."<br>";
-		}
-		
-	}else{
-		//echo "there is no such thing as addressee";
-	}
-	
-//for From* field:
-	if(isset($_GET["from"])){
-		
-		//only if there is from* in the URL
-		//echo "there is addresser";
-		
-		//if it's empty
-		if(empty($_GET["from"])){
-			//it is empty
-			echo "Please enter the addresser!";
-	}else{
-			//its not empty
-			echo "From: ".$_GET["from"]."<br>";
-		}
-		
-	}else{
-		//echo "there is no such thing as addressee";
-	}
-	
-//Getting the message from address
-//if there is ?name= .. then $_GET["name"]
-//$my_message = $_GET["message"];
-//$to = $_GET["to"];
-//$from = $_GET["from"];
-
-
-//echo "My message is ".$my_message." for"." ".$to." from "." ".$from;
-
-
-
 ?>
-
-<h2> First application </h2>
+<br><br>
+		
 
 <form method="get">
-<label for="message"> Message:*</label><br>
-<input type="text" name="message"><br>
-<label for="to"> To:*</label><br>
-<input type="text" name="to"><br>
-<label for="from"> From:*</label><br>
-<input type="from" name="from"><br>
-<input type="submit" value="Save to DB"> 
-
-	
-
+	<label for="age"> Age:*</label>
+	<input type="number" name="age" min="1" max="130" ><br>
 </form>
+
+<?php			
+	//for Age* field:
+	if(isset($_GET["age"])){
+		
+		//only if there is age typed* in the URL
+		//echo "there is age typed";
+		
+		//if it's empty
+		if(empty($_GET["age"])){
+			//it is empty
+			echo "Please enter your age!";
+	}else{
+			//its not empty
+			echo "Age: ".$_GET["age"]."<br>";
+		}
+		
+	}else{
+		//echo "there is no such thing as age typed";
+	}
+?>
+<br><br>
+
+<form method="get"
+	<label for="height"> Height (cm):*</label>
+	<input type="number" name="height" min="50" max="250"><br>
+</form>
+
+<?php	
+	//for Height* field:
+	if(isset($_GET["height"])){
+		
+		//only if there is height typed* in the URL
+		//echo "there is height typed";
+		
+		//if it's empty
+		if(empty($_GET["height"])){
+			//it is empty
+			echo "Please enter your height!";
+	}else{
+			//its not empty
+			echo "Height: ".$_GET["height"]."<br>";
+		}
+		
+	}else{
+		//echo "there is no such thing as height typed";
+	}
+	
+?>
+<br><br>
+
+<form method="get"
+	<label for="weight"> Weight (kg):*</label>
+	<input type="number" name="weight" min="15" max="600"><br><br>
+	<input type="submit" value="Get my BMI"> 
+</form>
+<?php	
+		//for Weight* field:
+	if(isset($_GET["weight"])){
+		
+		//only if there is weight typed* in the URL
+		//echo "there is weight typed";
+		
+		//if it's empty
+		if(empty($_GET["weight"])){
+			//it is empty
+			echo "Please enter your weight!";
+	}else{
+			//its not empty
+			echo "Weight: ".$_GET["weight"]."<br>";
+		}
+		
+	}else{
+		//echo "there is no such thing as weight typed";
+	}
+	
+?>
+
+
