071dd5643f44b67a765799a06c960ff4bb8c289e
askaks <askaks@greeny.cs.tlu.ee>
2016-02-25 11:46:18 +0200

    Kodune töö

---
+++ 4.php
@@ -0,0 +1,67 @@
+<?php
+	$everything_was_okay = true;
+	//*********************
+	// TO field validation
+	//*********************
+	if(isset($_GET["to"])){ //if there is ?to= in the URL
+		if(empty($_GET["to"])){ //if it is empty
+			$everything_was_okay = false; //empty
+			echo "Please enter the recipient! <br>"; // yes it is empty
+		}else{
+			echo "To: ".$_GET["to"]."<br>"; //no it is not empty
+		}
+	}else{
+		$everything_was_okay = false; // do not exist
+	}
+	//check if there is variable in the URL
+	if(isset($_GET["message"])){
+		
+		//only if there is message in the URL
+		//echo "there is message";
+		
+		//if its empty
+		if(empty($_GET["message"])){
+			//it is empty
+			$everything_was_okay = false;
+			echo "Please enter the message! <br>";
+		}else{
+			//its not empty
+			echo "Message: ".$_GET["message"]."<br>";
+		}
+		
+	}else{
+		//echo "there is no such thing as message";
+		$everything_was_okay = false;
+	}
+	
+	
+	
+	/***********************
+	**** SAVE TO DB ********
+	***********************/
+	
+	// ? was everything okay
+	if($everything_was_okay == true){
+		
+		echo "Saving to database ...";
+		
+	}
+	
+	
+?>
+
+<h2> First application </h2>
+
+<form method="get">
+	<label for="to">to:* <label>
+	<input type="text" name="to"><br><br>
+	
+	<label for="message">Message:* <label>
+	<input type="text" name="message"><br><br>
+	
+	<!-- This is the save button-->
+	<input type="submit" value="Save to DB">
+
+<form>
+
+<p>Idea</p>
